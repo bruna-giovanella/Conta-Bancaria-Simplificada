eefd53b31bed98e873825b796596ed865662eb1f
Bruna Giovanella <brugiovanella@gmail.com>
2025-04-20 20:18:14 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String nomeCliente = "Bruna Asquel Giovanella";
+        String tipoConta = "Corrente";
+        double saldo = 2500.00;
+
+        System.out.printf("""
+                Dados iniciais do cliente:
+                
+                Nome:           %s
+                Tipo Conta:     %s
+                Saldo Inicial:  %.2f
+                \n""", nomeCliente, tipoConta, saldo);
+
+        boolean sistemaAtivo = true;
+        while (sistemaAtivo) {
+            System.out.print("""
+                    \nOperações
+                    
+                    1- Consultar saldos
+                    2- Receber valor
+                    3- Transferir valor
+                    4- Sair
+                    
+                    Ação desejada: """);
+
+            int decisaoDoCliente = sc.nextInt();
+            switch (decisaoDoCliente) {
+                case 1:
+                    System.out.println("\nSeu saldo atual é de :" + saldo);
+                    break;
+                case 2:
+                    System.out.print("\nInforme o valor que será debitado: R$ ");
+                    double valorDebito = sc.nextDouble();
+                    saldo += valorDebito;
+                    break;
+                case 3:
+                    System.out.print("\nInforme o valor que será creditado: R$ ");
+                    double valorCredito = sc.nextDouble();
+                    saldo -= valorCredito;
+                    break;
+                case 4:
+                    System.out.println("\nSaindo...");
+                    sistemaAtivo = false;
+                    break;
+            }
+        }
+
+    }
+}
